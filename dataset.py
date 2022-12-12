1adabfa04a0ffa2eae12efbdb84e3db57cf85484
onions222 <huangwc0222@gmail.com>
2022-12-12 22:04:01 +0800

    add data_center.txt

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import Dataset
+import torchvision
 from torchvision import transforms
 from PIL import Image
 
@@ -22,7 +23,14 @@ class my_dataset(Dataset):
         img_path = data.split(' ')[0]
         img_data = Image.open(img_path)
         points = data.split(' ')[1:-2]
-        # 图像大小为100x100
+        # 图像大小为100x100，此处是对点进行归一化
         points = [int(i)/100 for i in points]
 
         return tf(img_data), torch.Tensor(points)
+
+
+if __name__ == '__main__':
+    data = my_dataset('dataset_center.txt')
+    for i in data:
+        print(i[0].shape)
+        print(i[1])
